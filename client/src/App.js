e44e8fd3149f2a85dc41fd32c4f2c890d0d39081
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-03-29 00:17:28 -0700

    Added startup api and client

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={apiResponse:""};
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              {this.state.apiResponse}
+            </p>
+            <a href="http://localhost:9000/testAPI" target="_blank">Check API</a>
+          </header>
+        </div>
+    );
+  }
+}
+
+export default App;
