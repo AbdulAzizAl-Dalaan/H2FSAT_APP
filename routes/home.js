84668b7c11b16e20e0485a3802ab73a6c3e4cfa4
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-09-13 20:20:04 -0700

    Added progress from views and routes files for survey models

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User')
 const Survey_Info = require('../models/Survey/Survey_Info')
 const Survey_Q = require('../models/Survey/Survey_Q')
 const Survey_A = require('../models/Survey/Survey_A')
-const Survey_R = require('../models/Survey/Survey_R')
+const Survey_R = require('../models/Survey/Survey_R');
+const e = require('express');
 
 const sessionChecker = (req, res, next) => {
   if(req.session.user)
@@ -51,16 +53,32 @@ router.get('/:id', async function(req, res, next) {
 });
 
 router.post('/:id/submit', async function(req, res, next) {
-  const survey = await Survey_Info.findByPk(req.params.id)
-  if (survey !== null)
+  if (req.query.msg)
   {
-    // put logic here to check results
-    console.log("INTO SUBMIT")
-    res.redirect('/home/?msg=success')
+    res.locals.msg = req.query.msg
+  }
+  const user = await User.findByPk(req.session.user.email);
+  if (user !== null)
+  {
+    const survey = await Survey_Info.findByPk(req.params.id)
+    console.log("ID: " + req.params.id)
+    console.log("USER: " + user)
+    console.log("SURVEY: " + survey)
+    const results = await Survey_R.findAll({where: {email: user.email, survey_id: survey.survey_id}})
+    if (results.length === 0)
+    {
+      const questions = await Survey_Q.findAll({where: {survey_id: req.params.id}})
+      const answers = await Survey_A.findAll({where: {survey_id: req.params.id}})
+      // PERFORM RESULT CREATION HERE
+    }
+    else 
+    {
+      res.redirect('/home/?msg=already')
+    }
   }
   else
   {
-    res.redirect('/home/?msg=notfound')
+    res.redirect('/?msg=raf')
   }
 });
 
