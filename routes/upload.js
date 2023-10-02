0492fb8c28b1e6a3a8d36edfb3f5db563bd631c0
AaronStraka <aaron.straka@wsu.edu>
2023-10-01 17:33:49 -0700

    Pushing a finished google form upload. Have a couple of bugs but overall works. I made a change to survey_info with the autoinc and isCSVdata

--- routes/upload.js
+++ routes/upload.js
@@ -15,20 +15,38 @@ const upload = multer({ storage: storage });
 router.get('/', (req, res) => {
     const isAdmin = true;  
     const isUnitLeader = true;
+    // if (!res.locals || !res.locals.email) {
+    //     return res.status(400).send("User is not logged in or email is not available.");
+    // }
+
+    
     res.render('upload', { isAdmin, isUnitLeader });
 });
 
 router.post('/handle-upload', upload.single('file'), async (req, res) => {
     try {
+
+        const uploaderEmail = req.session.user.email;
+
+        if (!uploaderEmail) {
+            return res.status(400).send("Uploader's email is not available.");
+        }
+
+        console.log("Uploader's Email:", uploaderEmail);
+
+
+
         // Check for user session
         // if (!req.user) {
         //     return res.status(400).send("User is not logged in.");
         // }
 
-        // Check if file exists
+        
         if (!req.file) {
             return res.status(400).send("No file uploaded.");
         }
+
+        
 
         const buffer = req.file.buffer;
         const workbook = xlsx.read(buffer, { type: 'buffer' });
@@ -36,8 +54,8 @@ router.post('/handle-upload', upload.single('file'), async (req, res) => {
         const sheet_name_list = workbook.SheetNames;
         const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
         
-        // Additional logging to diagnose issues
-        console.log("Excel Data:", xlData);
+        
+        console.log("Excel Data:", xlData);//see the data
 
         
 
@@ -51,19 +69,19 @@ router.post('/handle-upload', upload.single('file'), async (req, res) => {
         let questionIdMapping = {};  // Declare here to make it accessible later
 
         if (existingSurvey) {
-            // Use the survey_id of the existing survey
-            surveyId = existingSurvey.survey_id;
+            
+            surveyId = existingSurvey.survey_id;//use ID from past survey becasue it is the same
 
-            // Populate the questionIdMapping from the existing questions in the database
+            
             const existingQuestions = await Survey_Q.findAll({ where: { survey_id: surveyId } });
             existingQuestions.forEach(question => {
-                questionIdMapping[question.prompt] = question.question_id;
+                questionIdMapping[question.prompt] = question.question_id;//Populate the questionIdMapping from the existing questions in the database
             });
 
         } else {
-            // Create a new survey since it doesn't exist
+            //create a new survey since it doesn't exist
             const surveyData = {
-                author: "brian.harder@army.mil",
+                author: uploaderEmail,
                 title: title,
                 isCSVdata: true
             };
@@ -92,11 +110,11 @@ router.post('/handle-upload', upload.single('file'), async (req, res) => {
 
         for (let i = 0; i < xlData.length; i++) {
             const row = xlData[i];
-            const email = row[emailHeader];  // Using the found email header
+            const email = row[emailHeader];  
             let results = {};
 
             for (let header of headers) {
-                if (header !== emailHeader) {  // Using the found email header
+                if (header !== emailHeader) {  
                     const questionId = questionIdMapping[header];
                     results[questionId] = row[header];
                 }
