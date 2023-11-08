d51f802bb71080b50d2fc5848e6719821719a1ea
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-11-08 09:56:47 -0800

    Added fixes to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -38,6 +38,36 @@ router.post('/', async function(req, res, next) {
     }
     const user = await User.findOne({where: {email: req.session.user.email}});
     if (user) {
+      // req.body.dob stored at 2000-07-05 format yyyy-mm-dd check to see if date is at least 17 years old
+      const currentYear = new Date().getFullYear();
+      const dobYear = req.body.dob.split('-')[0];
+      const dobMonth = req.body.dob.split('-')[1];
+      const dobDay = req.body.dob.split('-')[2];
+      if (currentYear - dobYear < 17) {
+        res.redirect('/profile?msg=profile-date-error');
+        return;
+      }
+      else if (currentYear - dobYear == 17) {
+        const currentMonth = new Date().getMonth() + 1;
+        if (currentMonth < dobMonth) {
+          res.redirect('/profile?msg=profile-date-error');
+          return;
+        }
+        else if (currentMonth == dobMonth) {
+          const currentDay = new Date().getDate();
+          if (currentDay < dobDay) {
+            res.redirect('/profile?msg=profile-date-error');
+            return;
+          }
+        }
+      }
+
+      const unit = await Unit.findOne({where: {uic: req.body.unit, state: req.body.state}});
+      if (!unit) {
+        res.redirect('/profile?msg=profile-unit-error');
+        return;
+      }
+
       user.unit = req.body.unit;
       user.state = req.body.state;
       user.rank = req.body.rank;
