4037338973bec25a8603b5ab01e64326d49a9ca1
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2023-11-10 13:54:52 -0800

    Added db cert.

--- db.js
+++ db.js
@@ -1,11 +1,28 @@
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
 
-const sequelize = new Sequelize('new_database', 'root', 'Password123#@!', {
-    host: 'localhost',
+// Azure MySQL connection details
+const host = 'h2f-wsu-capstone.mysql.database.azure.com';
+const username = 'brian'; // Your Azure MySQL username
+const password = 'WSUcapstone#2023'; // Your Azure MySQL password
+const database = 'h2f_database'; // Your Azure MySQL database name
+const port = 3306; // Default MySQL port
+
+// SSL configuration
+const caCert = fs.readFileSync('DigiCertGlobalRootCA.crt.pem'); // Replace with your CA cert filename
+
+const sequelize = new Sequelize(database, username, password, {
+    host: host,
+    port: port,
     dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            ca: caCert
+        }
+    },
     define: {
         timestamps: false
     }
-})
+});
 
 module.exports = sequelize;
